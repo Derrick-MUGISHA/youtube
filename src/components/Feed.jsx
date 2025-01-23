56bcc1e8d43c466f809fec8c1398e771696bbac5
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2025-01-23 12:08:34 +0200

    new feeds

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,7 +21,7 @@ const Feed = () => {
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2022 JSM Media
+          Copyright © Derrick Mugisha 2025
         </Typography>
       </Box>
 
